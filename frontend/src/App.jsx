f56fe352905b2529618854b3dadba2f1d3088646
Steven <57573122+st4ven@users.noreply.github.com>
2024-09-07 22:26:17 -0700

    Adding employees via form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,58 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 import Header from './components/Header';
 import EmployeeList from './components/EmployeeList';
-import { getEmployees } from './EmployeeService';
+import { getEmployees, saveEmployee } from './EmployeeService';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const [data, setData] = useState({});
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    phone_number: '',
+    address: '',
+    job_title: '',
+    account_status: ''
+  });
+  const modalRef = useRef();
 
   const getAllEmployees = async () => {
     try {
       const { data } = await getEmployees();
       setData(data);
-      console.log(data);
     } catch (error) {
       console.error(error.message);
     }
   }
 
-  const toggleModal = (show) => { console.log("I was clicked") };
+  const handleNewEmployee = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await saveEmployee(values);
+
+      toggleModal(false);
+
+      setValues({
+        name: '',
+        email: '',
+        phone_number: '',
+        address: '',
+        job_title: '',
+        account_status: ''
+      });
+
+      getAllEmployees();
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  const toggleModal = show => show ? modalRef.current.showModal() : modalRef.current.close();
+
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  }
 
   useEffect(() => {
     getAllEmployees();
@@ -35,6 +69,47 @@ function App() {
           </Routes>
         </div>
       </main>
+
+      <dialog ref={modalRef} className="modal" id="modal">
+        <div className="modal__header">
+          <h3>New Employee</h3>
+          <i onClick={() => toggleModal(false)} className="bi bi-x-lg"></i>
+        </div>
+        <div className="modal__body">
+          <form onSubmit={handleNewEmployee}>
+            <div className="user-details">
+              <div className="input-box">
+                <span className="details">Name</span>
+                <input type="text" value={values.name} onChange={onChange} name='name' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input type="text" value={values.email} onChange={onChange} name='email' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Title</span>
+                <input type="text" value={values.job_title} onChange={onChange} name='job_title' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Phone Number</span>
+                <input type="text" value={values.phone_number} onChange={onChange} name='phone_number' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Address</span>
+                <input type="text" value={values.address} onChange={onChange} name='address' required />
+              </div>
+              <div className="input-box">
+                <span className="details">Account Status</span>
+                <input type="text" value={values.account_status} onChange={onChange} name='account_status' required />
+              </div>
+            </div>
+            <div className="form_footer">
+              <button onClick={() => toggleModal(false)} type='button' className="btn btn-danger">Cancel</button>
+              <button type='submit' className="btn">Save</button>
+            </div>
+          </form>
+        </div>
+      </dialog>
     </>
   )
 }
